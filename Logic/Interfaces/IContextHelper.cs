06238671a5bd4b186b0f90f1ed9153d489427cab
Andy Flynn <andyf1ynn@yahoo.co.uk>
2016-06-21 10:19:15 +0100

    Demo project

---
+++ Logic/Interfaces/IContextHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
+
+namespace Core.Interfaces
+{
+    interface IContextHelper<T>
+    {
+        bool Insert<T>(T row) where T : class;
+        bool Update<T>(T row) where T : class;
+        bool Delete<T>(T row) where T : class;
+        bool AddRows<T>(T[] rows) where T : class;
+    }
+}
